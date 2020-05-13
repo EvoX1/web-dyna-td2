4d2823c88200f414a27eed3f918146ab2ec260a4
EvoX1 <50424148+EvoX1@users.noreply.github.com>
2020-05-13 09:58:26 +0200

    commit 1/2 H TP4

--- tp4/app/controllers/SambleController.php
+++ tp4/app/controllers/SambleController.php
@@ -1,5 +1,11 @@
 <?php
 namespace controllers;
+
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\URequest;
+use Ubiquity\utils\http\UResponse;
+use Ubiquity\utils\http\USession;
+
  /**
  * Controller SambleController
  **/
@@ -11,6 +17,13 @@ class SambleController extends ControllerBase{
     public function Inscription(){
 
         $this->loadView("SambleController/Inscription.html");
+
+    }
+        
+    public function Inscription2(){
+
+    UResponse::header("location" ,"/");
+
 
     }
 }
